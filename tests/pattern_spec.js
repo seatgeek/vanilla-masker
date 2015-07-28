95a2696853462045b82ae3dd79f911bcf0089f1f
Gareth Tan <garethwtan@gmail.com>
2015-07-28 18:39:35 -0400

    Add unmaskPattern and tests.

--- tests/pattern_spec.js
+++ tests/pattern_spec.js
@@ -99,6 +99,79 @@ describe("VanillaMasker.toPattern", function() {
   it('returns "(aaa) _____" when input is aaa999aaaa and placeholder is _', function(){
     expect(VMasker.toPattern('aaa999aaaa', {pattern: "(AAA) AAAAA", placeholder: "_"})).toEqual('(aaa) _____');
   });
+});
 
+describe("VanillaMasker.unmaskPattern", function() {
 
+  it('returns "(61)" pattern when input is 61', function() {
+    expect(VMasker.unmaskPattern('(61)', '(99)')).toEqual('61');
+  });
+
+  it('returns "(61) 91234-5678" pattern when input is 61912345678', function() {
+    expect(VMasker.unmaskPattern('(61) 91234-5678', '(99) 99999-9999')).toEqual('61912345678');
+  });
+
+  it('returns "(10) 9991-1111" pattern when input is 1099911111', function() {
+    expect(VMasker.unmaskPattern('(10) 9991-1111', '(99) 9999-9999')).toEqual('1099911111');
+  });
+
+  it('returns "(10) 11" pattern when input is 1011', function() {
+    expect(VMasker.unmaskPattern('(10) 11', '(99) 9999-9999')).toEqual('1011');
+  });
+
+  it('returns "+10 11 4444-44" pattern when input is 1011444444', function() {
+    expect(VMasker.unmaskPattern('+10 11 4444-44', '+99 99 9999-99')).toEqual('1011444444');
+  });
+
+  it('returns "12/12/2000" pattern when input is 12122000', function() {
+    expect(VMasker.unmaskPattern('12/12/2000', '99/99/9999')).toEqual('12122000');
+  });
+
+  it('returns "10/11" pattern when input is 1011', function() {
+    expect(VMasker.unmaskPattern('10/11', '99/99/9999')).toEqual('1011');
+  });
+
+  it('returns "2000/12/12" pattern when input is 20001212', function() {
+    expect(VMasker.unmaskPattern('2000/12/12', '9999/99/99')).toEqual('20001212');
+  });
+
+  it('returns "999.111.111-01" pattern when input is 99911111101', function() {
+    expect(VMasker.unmaskPattern('999.111.111-01', '999.999.999-99')).toEqual('99911111101');
+  });
+
+  it('returns "101.1" pattern when input is 1011', function() {
+    expect(VMasker.unmaskPattern('101.1', '999.999.999-99')).toEqual('1011');
+  });
+
+  it('returns "ABC-1234" pattern when input is ABC1234', function() {
+    expect(VMasker.unmaskPattern('ABC-1234', 'AAA-1234')).toEqual('ABC');
+  });
+
+  it('returns incomplete result: "AB" when input is AB1 and pattern is AAA-99', function() {
+    expect(VMasker.unmaskPattern('AB', 'AAA-99')).toEqual('AB');
+  });
+
+  it('returns "9B.GR.D08X0.4.G.117974" pattern when input is 9BGRD08X04G117974', function() {
+    expect(VMasker.unmaskPattern('9B.GR.D08X0.4.G.117974', 'SS.SS.SSSSS.S.S.SSSSSS')).toEqual('9BGRD08X04G117974');
+  });
+
+  it('returns "BB.G" pattern when input is 9BG', function() {
+    expect(VMasker.unmaskPattern('BB.G', 'SS.SS.SSSSS.S.S.SSSSSS')).toEqual('BBG');
+  });
+
+  it('returns "(4xx) xxx-xxxx" when input is 4 and placeholder is x', function(){
+    expect(VMasker.unmaskPattern('(4xx) xxx-xxxx', {pattern: "(999) 999-9999", placeholder: "x"})).toEqual('4');
+  });
+
+  it('returns "(___) ___-_____" when input is empty and placeholder is _', function(){
+    expect(VMasker.unmaskPattern('(___) ___-____', {pattern: "(999) 999-9999", placeholder: "_"})).toEqual('');
+  });
+
+  it('returns "(111) 111-1111" when input is 1111111111 and placeholder is _', function(){
+    expect(VMasker.unmaskPattern('(111) 111-1111', {pattern: "(999) 999-9999", placeholder: "_"})).toEqual('1111111111');
+  });
+
+  it('returns "(aaa) _____" when input is aaa999aaaa and placeholder is _', function(){
+    expect(VMasker.unmaskPattern('(aaa) _____', {pattern: "(AAA) AAAAA", placeholder: "_"})).toEqual('aaa');
+  });
 });
